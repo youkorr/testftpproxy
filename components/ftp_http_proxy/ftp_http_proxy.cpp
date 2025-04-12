09331fa26a485c372b484dcd9b772611354be8e1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-12 21:36:33 +0200

    Update ftp_http_proxy.cpp

--- components/ftp_http_proxy/ftp_http_proxy.cpp
+++ components/ftp_http_proxy/ftp_http_proxy.cpp
@@ -150,7 +150,7 @@ bool FTPHTTPProxy::download_file(const std::string &remote_path, httpd_req_t *re
 
   bool is_media_file = (extension == ".mp3" || extension == ".mp4" || 
                         extension == ".wav" || extension == ".ogg" || 
-                        extension == ".png" ||extension == ".jpg");
+                        extension == ".png" ||extension == ".jpg"  ||
                         extension == ".avi");
 
   // Réduire encore plus la taille du buffer pour les fichiers média
