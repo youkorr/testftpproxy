510ffa79e37600e8dc91b1aba486dd65ee7e2cb8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-13 00:14:24 +0200

    Update ftp_http_proxy.cpp

--- components/ftp_http_proxy/ftp_http_proxy.cpp
+++ components/ftp_http_proxy/ftp_http_proxy.cpp
@@ -149,7 +149,7 @@ bool FTPHTTPProxy::download_file(const std::string &remote_path, httpd_req_t *re
                         extension == ".wav" || extension == ".ogg");
 
   // Réduire encore plus la taille du buffer pour les fichiers média
-  int buffer_size = is_media_file ? 8192 : 16384;
+  int buffer_size = is_media_file ? 16384 : 32768;
   
   // Allouer le buffer en SPIRAM
   char* buffer = (char*)heap_caps_malloc(buffer_size, MALLOC_CAP_SPIRAM);
