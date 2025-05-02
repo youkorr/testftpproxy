2e87126098cfe24af89eead27a37b07e093fd0b5
youkorr <96953496+youkorr@users.noreply.github.com>
2025-05-02 16:39:05 +0200

    Update __init__.py

--- components/sd_web_server/__init__.py
+++ components/sd_web_server/__init__.py
@@ -1,27 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import web_server_base
 from esphome.const import CONF_ID, CONF_PORT
 
-CODEOWNERS = ['@votre_utilisateur']
-DEPENDENCIES = ['sd_mmc_card']
+CODEOWNERS = ["@ton_pseudo"]
+DEPENDENCIES = ["sd_mmc_card"]
 
-sd_web_server_ns = cg.esphome_ns.namespace('sd_web_server')
-SDWebServer = sd_web_server_ns.class_('SDWebServer', cg.Component, web_server_base.WebServerBase)
+sd_web_server_ns = cg.esphome_ns.namespace("sd_web_server")
+SDWebServer = sd_web_server_ns.class_("SDWebServer", cg.Component)
 
 CONF_SD_DIR = "sd_dir"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SDWebServer),
     cv.Optional(CONF_PORT, default=8080): cv.port,
-    cv.Optional(CONF_SD_DIR, default="/sdcard"): cv.string,  # Ajout du champ ici
+    cv.Optional(CONF_SD_DIR, default="/sdcard"): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     cg.add(var.set_port(config[CONF_PORT]))
-    yield web_server_base.web_server_base_to_code(var)
-    cg.add(var.set_sd_directory(config[CONF_SD_DIR]))  # Utilisation du champ ici
+    cg.add(var.set_sd_directory(config[CONF_SD_DIR]))
 
 
