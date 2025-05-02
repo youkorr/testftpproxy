691351f59e96dcdebd385cdf817e36a1097937ab
youkorr <96953496+youkorr@users.noreply.github.com>
2025-05-02 16:27:32 +0200

    Update __init__.py

--- components/sd_web_server/__init__.py
+++ components/sd_web_server/__init__.py
@@ -9,6 +9,7 @@ DEPENDENCIES = ['sd_mmc_card']
 sd_web_server_ns = cg.esphome_ns.namespace('sd_web_server')
 SDWebServer = sd_web_server_ns.class_('SDWebServer', cg.Component, web_server_base.WebServerBase)
 
+CONF_SD_DIR = "sd_dir"
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SDWebServer),
     cv.Optional(CONF_PORT, default=8080): cv.port,
@@ -19,4 +20,5 @@ def to_code(config):
     yield cg.register_component(var, config)
     cg.add(var.set_port(config[CONF_PORT]))
     yield web_server_base.web_server_base_to_code(var)
+    cg.add(var.set_sd_directory(config[CONF_SD_DIR]))
 
